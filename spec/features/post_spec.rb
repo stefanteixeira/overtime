4f432fbc7e0c6aaa0275c7a45d7a4ec46aa1383f
stefanteixeira <stefanfk@gmail.com>
2016-11-01 01:02:01 -0200

    Implement user association with posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,8 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      user = User.create(email: 'test@test.com', password: 'abc123', password_confirmation: 'abc123', first_name: 'Wesley', last_name: 'Safadão')
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -25,10 +27,17 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
-
       click_on 'Save'
 
       expect(page).to have_content('Some rationale')
+    end
+
+    it 'will have user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'User Association'
+      click_on 'Save'
+
+      expect(User.last.posts.last.rationale).to eq('User Association')
     end
   end
 end
