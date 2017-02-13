030e3739536e46121708061546d0c8bfa85f6935
stefanteixeira <stefanfk@gmail.com>
2017-02-13 00:28:20 -0200

    Add overtime hours to post model

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'navigate' do
   let(:user) { FactoryGirl.create(:user) }
 
-  let(:post) { Post.create(date: Date.today, rationale: 'Rationale', user_id: user.id) }
+  let(:post) { Post.create(date: Date.today, rationale: 'Rationale', user_id: user.id, overtime_request: 3.5) }
 
   before do
     login_as(user, :scope => :user)
@@ -31,7 +31,7 @@ describe 'navigate' do
 
     it 'has a scope so that only post creators can see their posts' do
       other_user = User.create(first_name: 'Other', last_name: 'User', email: 'otheruser@test.com', password: 'other123', password_confirmation: 'other123')
-      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
 
       visit posts_path
 
@@ -60,14 +60,16 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
-      click_on 'Save'
+      fill_in 'post[overtime_request]', with: 4.5
 
-      expect(page).to have_content('Some rationale')
+      expect { click_on 'Save' }.to change(Post, :count).by(1)
     end
 
     it 'will have user associated with it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'User Association'
+      fill_in 'post[overtime_request]', with: 4.5
+
       click_on 'Save'
 
       expect(User.last.posts.last.rationale).to eq('User Association')
@@ -103,7 +105,7 @@ describe 'navigate' do
       delete_user = FactoryGirl.create(:user)
       login_as(delete_user, :scope => :user)
 
-      post_to_delete = Post.create(date: Date.today, rationale: 'huehue', user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: 'huehue', user_id: delete_user.id, overtime_request: 3.5)
 
       visit posts_path
 
