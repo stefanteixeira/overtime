809e59b2bc8a4fb1faf0907758dc6f8892dc53d0
stefanteixeira <stefanfk@gmail.com>
2017-02-16 04:09:31 -0200

    Block non admin users from accessing audit logs page

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -19,7 +19,13 @@ describe 'AuditLog Feature' do
     end
 
     it 'cannot be accessed by non-admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
 
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
