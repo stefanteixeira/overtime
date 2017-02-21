94870844554de13a63d9484c644ab4c9336ff025
stefanteixeira <stefanfk@gmail.com>
2017-02-21 03:41:50 -0300

    Remove sms libs and related stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,5 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.before(:each) do
-    stub_const('SmsTool', FakeSms)
-  end
-
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
